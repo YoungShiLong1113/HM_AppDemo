6f0520efb7ad63bd045caa3de8f560d9632109eb
YoungShiLong1113 <128705241+YoungShiLong1113@users.noreply.github.com>
2024-06-06 13:32:58 +0800

    登陆页面

---
+++ entry/src/main/ets/entryability/test.ts
@@ -0,0 +1,12 @@
+//set
+let nameSet=new Set();
+let nameSet1=new Set(["james","john","jerry"])
+{
+  console.log(nameSet1);
+}
+
+let myMap = new Map();
+let myMap1 = new Map([101,"jerry"][102,"tom"])
+{
+
+}
